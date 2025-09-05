7ec437105fd0e6e76a18d6c66d70147674651edd
sanyxmm <iamsanyamchoudhary@gmail.com>
2025-09-05 15:55:29 +0530

    config updated

--- main-app/vite.config.js
+++ main-app/vite.config.js
@@ -1,31 +1,35 @@
 // vite.config.js
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from '@originjs/vite-plugin-federation'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: 'main_app',
-      remotes: {
-        // later we’ll add "music_library" micro frontend here
-        music_library: "http://localhost:5001/assets/remoteEntry.js"
-      },
-      shared: ['react', 'react-dom']
-    })
-  ],
-  server: {
-    port: 5000,
-  },
-  build: {
-    target: 'esnext',
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`,
+export default defineConfig(({ mode }) => {
+ 
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      react(),
+      federation({
+        name: 'main_app',
+        remotes: {
+          music_library: env.VITE_MUSIC_LIBRARY_URL,
+        },
+        shared: ['react', 'react-dom'],
+      }),
+    ],
+    server: {
+      port: 5000,
+    },
+    build: {
+      target: 'esnext',
+      rollupOptions: {
+        output: {
+          entryFileNames: `assets/[name].js`,
+          chunkFileNames: `assets/[name].js`,
+          assetFileNames: `assets/[name].[ext]`,
+        },
       },
     },
-  },
+  }
 })
